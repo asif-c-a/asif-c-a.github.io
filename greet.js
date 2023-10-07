e8558b2902789d4b5950eaa498b5c7d50cf66836
Asif C.A <111337174+asif-c-a@users.noreply.github.com>
2023-10-07 18:12:02 +0530

    Update greet.js

--- greet.js
+++ greet.js
@@ -6,8 +6,6 @@ if (hour < 12)
 	document.getElementById("greeting").innerHTML = "GOOD MORNING";
 }else if (hour > 12 && hour < 18) {
 	document.getElementById("greeting").innerHTML = "GOOD AFTERNOON";
-}else if (hour > 18 && hour < 24) {
-	document.getElementById("greeting").innerHTML = "GOOD EVENING";
 }else{
-	document.getElementById("greeting").innerHTML = "DUDE! WHAT ARE YOU DOING?!";
+	document.getElementById("greeting").innerHTML = "GOOD EVENING";
 }
